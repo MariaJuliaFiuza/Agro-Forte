490e4d5b0adf09c6dad83dd3d077c015bb1b6f8b
Maria Julia Fiuza <m.gula@escola.pr.gov.br>
2026-06-11 11:43:19 -0300

    .

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-// Seleciona o botão de acessibilidade
-const accessibilityBtn = document.getElementById('accessibility-btn');
+const button = document.getElementById('acessibilidade');
+const body = document.body;
 
-// Define um estado para alternar o tamanho da fonte
-let isLarge = false;
-
-accessibilityBtn.addEventListener('click', () => {
-    document.body.classList.toggle('large-text');
-    isLarge = !isLarge;
-
-    // Muda o texto do botão de acordo com o estado
-    accessibilityBtn.textContent = isLarge ? 'Reduzir Fonte' : 'Aumentar Fonte';
+button.addEventListener('click', () => {
+    body.classList.toggle('accessible');
 });

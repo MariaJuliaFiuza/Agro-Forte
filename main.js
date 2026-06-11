779a6d87340c7cf6505c3b1f518738f3597b7485
Maria Julia Fiuza <m.gula@escola.pr.gov.br>
2026-06-11 12:00:54 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-const button = document.getElementById('acessibilidade');
-const body = document.body;
+let pontos = 0;
+let respondidas = [];
 
-button.addEventListener('click', () => {
-    body.classList.toggle('accessible');
-});
+function verificar(pergunta, resposta){
+
+    if(respondidas.includes(pergunta)){
+        return;
+    }
+
+    respondidas.push(pergunta);
+
+    if(
+        (pergunta === 1 && resposta === 'a') ||
+        (pergunta === 2 && resposta === 'a') ||
+        (pergunta === 3 && resposta === 'a') ||
+        (pergunta === 4 && resposta === 'a') ||
+        (pergunta === 5 && resposta === 'a')
+    ){
+        pontos++;
+    }
+
+    if(respondidas.length === 5){
+        document.getElementById("resultado").innerHTML =
+        `🌱 Você acertou ${pontos} de 5 perguntas!`;
+    }
+}
